6929dbc249af59f934c171755bb1af01641d9db6
sandarunihara <sandarunihara56@gmail.com>
2024-07-20 20:09:38 +0530

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactUs />} />
         </Routes>
+        {/* done */}
       </Router>
       </ThemeProvider>
   );
